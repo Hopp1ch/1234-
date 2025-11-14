6e459106a3c7d4fe1d7e3ed5ba592149c0481383
Hopp1ch <ya.korolgovna228@gmail.com>
2025-11-14 10:25:00 +0300

    Update 9 work.cpp

--- 9 work.cpp
+++ 9 work.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
 	setlocale(0, "");
-	cout << "[~] ЦИКЛ WHILE" << endl;
+	cout << "[~] ЦИКЛ WHILE\n";
 	cout << "Для выхода из программы нажмите на 0\n";
 	int number;
 	do {
@@ -10,7 +10,7 @@ int main() {
 		cin >> number;
 		if (number >= 1 and number <= 15) {
 			for (int i = 0; i != number; i++) {
-				cout << "Цикл отработал. Круг:" << i+1 << endl;
+				cout << "Цикл отработал. Круг:" << i+1 << "\n";
 			}
 		}
 		else if (number < 1 and number != 0) {
