21965704c5981c3d41cf1d89f38d703cb035fc7b
Hopp1ch <ya.korolgovna228@gmail.com>
2025-11-14 10:11:13 +0300

    Update 9 work.cpp

--- 9 work.cpp
+++ 9 work.cpp
@@ -3,21 +3,23 @@ using namespace std;
 int main() {
 	setlocale(0, "");
 	cout << "[~] ЦИКЛ WHILE" << endl;
-	cout << "Для выхода из программы нажмите на 0" << endl;
+	cout << "Для выхода из программы нажмите на 0\n";
 	int number;
 	do {
 		cout << "Введите число: ";
 		cin >> number;
 		if (number >= 1 and number <= 15) {
-			cout << "Цикл отработал. Круг:" << number << endl;
+			for (int i = 0; i != number; i++) {
+				cout << "Цикл отработал. Круг:" << i+1 << endl;
+			}
 		}
 		else if (number < 1 and number != 0) {
-			cout << "Цикл не может отработать если значения меньше единицы" << endl;
-			cout << "Повторите операцию" << endl;
+			cout << "Цикл не может отработать если значения меньше единицы\n";
+			cout << "Повторите операцию\n";
 		}
 		else if(number > 15 and number !=0){
-			cout << "Цикл не может отработать если значение больше пятнадцати" << endl;
-			cout << "Повторите операцию" << endl;
+			cout << "Цикл не может отработать если значение больше пятнадцати\n";
+			cout << "Повторите операцию\n";
 		}
 	}while (number != 0);
 	return 0;
